72d055f238881b59e32b2b930c35e5d49b35a4c3
twwli <hello@theworldwelivein.co.uk>
2023-05-30 09:00:56 +0200

    Initial Commit

---
+++ email-notification-on-post.php
@@ -0,0 +1,28 @@
+<?php
+/**
+* Plugin Name: Email Notification on Post
+* Description: Envoie un e-mail lorsqu'un nouvel article est publié
+* Version: 1.0
+* Author: Votre nom
+* Author URI: https://votresite.com/
+*/
+
+function send_email_notification($post_ID)  {
+    $post = get_post($post_ID);
+    $author = get_userdata($post->post_author);
+
+    $subject = "Auf Ihrer Seite wurde ein neuer Artikel veröffentlicht.";
+    $message = "Auf Ihrer Seite wurde ein neuer Artikel veröffentlicht. Bitte überprüfen Sie ihn :\n\n";
+    $message .= $post->post_title . "\n\n";
+    $message .= get_permalink($post_ID);
+
+    wp_mail('rss-reminder@floatmagazin.de', $subject, $message);
+
+    return $post_ID;
+}
+
+add_action('publish_post', 'send_email_notification');
+add_action('publish_boote', 'send_email_notification');
+add_action('publish_dinge', 'send_email_notification');
+add_action('publish_orte', 'send_email_notification');
+add_action('publish_leute', 'send_email_notification');
