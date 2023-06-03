39c906bab98b1a0e68c822a44635aff6d3bf0261
twwli <hello@theworldwelivein.co.uk>
2023-06-03 07:34:21 +0200

    Update 1.1
    Does not send emails on modifications

--- email-notification-on-post.php
+++ email-notification-on-post.php
@@ -1,15 +1,20 @@
 <?php
 /**
 * Plugin Name: Email Notification on Post
-* Description: Envoie un e-mail lorsqu'un nouvel article est publié
-* Version: 1.0
-* Author: Votre nom
-* Author URI: https://votresite.com/
+* Description: Send an email when a new article is published
+* Version: 1.1
+* Author: Olivier Guillard
+* Author URI: https://floatmagazin.de
 */
 
 function send_email_notification($post_ID)  {
     $post = get_post($post_ID);
     $author = get_userdata($post->post_author);
+
+    // Ignore les modifications d'articles déjà publiés
+    if ($post->post_date_gmt != $post->post_modified_gmt) {
+        return;
+    }
 
     $subject = "Bitte schaue dir diesen Artikel an.";
     $message = "Bitte schaue dir diesen Artikel an. Bitte überprüfen Sie ihn :\n\n";
